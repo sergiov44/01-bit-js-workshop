77071c318fd59709d040915d2f62a6a9e989cbf9
Sergio Vergara <sergiovergara123@gmail.com>
2023-10-18 17:05:25 -0500

    Exercise 01 in Folder 06 completed

--- 00-fundamentals/06-switch/01-grades-average.js
+++ 00-fundamentals/06-switch/01-grades-average.js
@@ -1,16 +1,23 @@
-// Crea un programa que solicite al usuario tres notas calcular el promedio. El rango permitido es de 0 a 5, incluidos decimales. Utilice una estructura switch para mostrar un mensaje correspondiente a la calificación ingresada, así:
-
-// Si saca menos de 2.5 ( 'Insuficiente, has perdido la materia' )
-// Si saca 2.5 y de menos de 3.5 ( 'Insuficiente, tienes derecho a habilitar' )
-// Si saca 3.5 y menos de 4.0 ( 'Aceptable' )
-// Si saca 4.1 y hasta 5.0 ( 'Excelente' )
-
 // 01 Prompt for 3 grades within a 0 to 5 range, including decimal numbers, to calculate their average and show the appropriate message
 
-let grades = parseFloat(prompt("Type 3 grades separated with commas: "));
-let gradesList = grades.split(",");
-let average = (gradesList[0] + gradesList[1] + gradesList[2]) / gradesList.length;
+let grade1 = parseFloat(prompt("Please type the first grade: "));
+let grade2 = parseFloat(prompt("Please type the second grade: "));
+let grade3 = parseFloat(prompt("Please type the third grade: "));
+let average = (grade1 + grade2 + grade3) / 3;
 
 switch(average) {
-    case 1:
+    case average >= 0 && average > 2.5:
+        console.log("Insufficient, you did not pass the course.");
+        break;
+    case average >= 2.5 && average < 3.5:
+        console.log("Insufficient, you can repeat the test.");
+        break;
+    case average >= 3.5 && average < 4:
+            console.log("Course passed.");
+            break;
+    case average >= 4:
+        console.log("Excellent!");
+        break;
+    default:
+        console.log("Please type three numberical grades, between 0.0 and 5.0.");
 }
